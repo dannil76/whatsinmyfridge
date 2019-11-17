a9ec1c0c7c298b0271e00dc03baca9374e02e59f
Dan <mail@dannilsson.se>
2019-11-17 22:58:12 +0100

    add mocks data

---
+++ src/mocks/Products.js
@@ -0,0 +1,134 @@
+const Products = [
+  {
+    name: 'Vetemjöl',
+    category: 'Skafferi',
+    stocked: true,
+  },
+  {
+    name: 'Havregryn',
+    category: 'Skafferi',
+    stocked: true,
+  },
+  {
+    name: 'Kyckling (hel)',
+    category: 'Fågel',
+    stocked: true,
+  },
+  {
+    name: 'Socker',
+    category: 'Skafferi',
+    stocked: false,
+  },
+  {
+    name: 'Blandfärs',
+    category: 'Kött',
+    stocked: true,
+  },
+  {
+    name: 'Nötfärs',
+    category: 'Kött',
+    stocked: false,
+  },
+  {
+    name: 'Fläskfilé',
+    category: 'Kött',
+    stocked: true,
+  },
+  {
+    name: 'Kycklingfilé',
+    category: 'Fågel',
+    stocked: true,
+  },
+  {
+    name: 'Kycklingklubba',
+    category: 'Fågel',
+    stocked: true,
+  },
+  {
+    name: 'Ströbröd',
+    category: 'Skafferi',
+    stocked: true,
+  },
+  {
+    name: 'Skinka',
+    category: 'Pålägg',
+    stocked: true,
+  },
+  {
+    name: 'Ost',
+    category: 'Pålägg',
+    stocked: true,
+  },
+  {
+    name: 'Skinka (rökt)',
+    category: 'Pålägg',
+    stocked: true,
+  },
+  {
+    name: 'Bacone',
+    category: 'Kött',
+    stocked: true,
+  },
+  {
+    name: 'Spagetti',
+    category: 'Skafferi',
+    stocked: true,
+  },
+  {
+    name: 'Lasagneplattor',
+    category: 'Skafferi',
+    stocked: true,
+  },
+  {
+    name: 'Ris',
+    category: 'Skafferi',
+    stocked: true,
+  },
+  {
+    name: 'Bröd',
+    category: 'Bak',
+    stocked: true,
+  },
+  {
+    name: 'Mjölk',
+    category: 'Mejeri',
+    stocked: true,
+  },
+  {
+    name: 'Bredgott',
+    category: 'Mejeri',
+    stocked: true,
+  },
+  {
+    name: 'Smör',
+    category: 'Mejeri',
+    stocked: true,
+  },
+  {
+    name: 'Köttbuljong',
+    category: 'Kryddor',
+    stocked: true,
+  },
+  {
+    name: 'Fiskbuljong',
+    category: 'Kryddor',
+    stocked: true,
+  },
+  {
+    name: 'Salt',
+    category: 'Kryddor',
+    stocked: true,
+  },
+  {
+    name: 'Peppar (svart)',
+    category: 'Kryddor',
+    stocked: true,
+  },
+  {
+    name: 'Peppar (vit)',
+    category: 'Kryddor',
+    stocked: true,
+  },
+];
+
+export default Products;
