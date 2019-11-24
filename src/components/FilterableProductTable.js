69144c82d592ab6a21ffd37d10a0f68bac1cdf9d
Dan <mail@dannilsson.se>
2019-11-24 10:04:57 +0100

    rename file

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
-import useProducts from '../productsDb';
+import useProducts from '../productsHook';
 
 function FilterableProductTable() {
 
