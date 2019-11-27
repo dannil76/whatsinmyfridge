cd4b21d458473f39206972279c1cd488477c9eae
Dan <mail@dannilsson.se>
2019-11-27 22:42:28 +0100

    add modal, add product form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 import FilterableProductTable from './components/FilterableProductTable';
+import AddProductForm from './components/AddProductForm';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModal = () => setShowModal(!showModal);
+
   return (
-    <Container className="mt-4">
-      <Row className="justify-content-md-center">
-        <Col md="auto">
-          <FilterableProductTable />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container className="mt-4">
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <FilterableProductTable />
+            <Button variant="info" onClick={handleModal}>
+              Lägg till ingrediens
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <Modal show={showModal} onHide={handleModal}>
+        <AddProductForm />
+      </Modal>
+    </>
   );
 }
 
