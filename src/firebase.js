a4102ae0666477b992a3b89a064c107570e7746f
Dan Nilsson <mail@dannilsson.se>
2020-03-16 07:44:40 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyB89B2otwWxJHaeTIeUy_ifqTEfdj7Y-qY',
+  apiKey: '',
   authDomain: 'whatsinmykitchen76.firebaseapp.com',
   databaseURL: 'https://whatsinmykitchen76.firebaseio.com',
   projectId: 'whatsinmykitchen76',
