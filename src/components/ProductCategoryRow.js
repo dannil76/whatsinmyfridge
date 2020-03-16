ebd4bc4f2a8cdd2ed63d25feb670cfd0a2ec1629
Dan <mail@dannilsson.se>
2020-03-16 07:34:19 +0100

    add colSpan

--- src/components/ProductCategoryRow.js
+++ src/components/ProductCategoryRow.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductCategoryRow({ category }) {
   return (
     <tr>
-      <td style={{ backgroundColor: '#000', fontSize: '70%', padding: '5px 10px' }}>
+      <td colSpan="2" style={{ backgroundColor: '#000', fontSize: '70%', padding: '5px 10px' }}>
         {category}
       </td>
     </tr>
